ba10ecd1e8826cc4d043054ce5d4b1a87e0ee7c7
Advait Joshi <joshiadvait8@gmail.com>
2020-03-19 20:40:50 +0530

    about changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -25,8 +25,7 @@ const Header = () => {
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            {title || "Hi 👋, I'm "} <span className="text-color-main">{name || 'Your Name'}</span>
             <br />
             {subtitle || "I'm the Unknown Developer."}
           </h1>
--- src/mock/data.js
+++ src/mock/data.js
@@ -11,7 +11,7 @@ export const headData = {
 export const heroData = {
   title: '',
   name: 'Advait Joshi',
-  subtitle: 'FullStack Developer',
+  subtitle: ', a web and mobile app developer',
   cta: '',
 };
 
@@ -19,9 +19,9 @@ export const heroData = {
 export const aboutData = {
   img: 'jpg.jpg',
   paragraphOne:
-    " I live in Mumbai I have completed my graduation in science from Birla College majoring in Information Technology. This is the place where interest in programming created in me further I 've decided to go ahead so I'm into to K J Somaiya Institute of Management Studies & Research popularly known as SIMSR one of the prestigious colleges in Mumbai doing masters in computer applications here I've learned many new things as well as brushed up my old technical skill.",
+    "I am a full stack developer & currently working as an Intern in Asian Paints. I've very good knowledge of Web development and developed handful of websites for college fests to make a digital presence.",
   paragraphTwo:
-    "On weekends I love to attend hackathons still I've attended only 3 they are the place where I've got to connect with techie public view awesome ideas and Last but not least learn something at a higher pace Other than coding, programming, technology I love to hang out with friends also I used to play badminton, Table tennis some times that acts as refreshment.",
+    'I have good knowledge of backend technologies like Java, Python, flask for REST APIs and JavaScript that is used to solve the business problem also with that frontend knowledge of HTML CSS and React native for developing good UI/UX for the users in web and mobile apps for both iOS and Android.',
   paragraphThree: '',
   resume:
     'https://uploads.codesandbox.io/uploads/user/3ecae8ee-0760-4738-930e-fbfb05fc34ec/MzyT-Advait_Joshi_Resume.pdf',
